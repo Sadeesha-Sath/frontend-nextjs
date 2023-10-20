0f7d311faf5a7840829d9883226b66c9b42e9799
Sadeesha-Sath <sadeerc@gmail.com>
2023-10-20 17:51:48 +0530

    Add more views and env config

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+const { Row, Typography } = require("antd")
+
+const { Title, Text } = Typography;
+
+const Header = () => {
+    return (
+        <Row >
+            <Title >A Bank</Title>
+            
+        </Row>
+    )
+};
