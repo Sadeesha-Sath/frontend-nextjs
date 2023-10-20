522619e99dad45f8788d14bf73226d0f6075221a
cneluhena <129386035+cneluhena@users.noreply.github.com>
2023-10-20 09:56:29 +0530

    changes to reports

--- src/app/reports/totaltransaction/page.js
+++ src/app/reports/totaltransaction/page.js
@@ -1,80 +1,149 @@
 "use client"
-import React from 'react';
-import { Table } from 'antd';
-import './style.css';
+import React, { useEffect, useState} from 'react';
+import { Select, Input, Form, Button, Table} from 'antd';
+import "./style.css" 
 
+
+  const branch_fetch = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/branches");
+        const json = await response.json();
+        const branches = json.map(item => ({
+          value: item.BranchID,
+          label: item.BranchName
+        }));
+        return branches;
+
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+  const view_fetch = async ()=>{
+    try {
+      const response = await fetch("http://localhost:8080/view");
+      const json = await response.json();
+      console.log(json);
+      const view = json.map((item, index) => ({
+        accnum: item.AccountNo,
+        count: item.Count, 
+        key: (index+1).toString()
+      }));
+      return view;
+
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  
+const Report = () => {
+  const [reportType, setReportType] = useState(null);
+  const [branch, setBranch] = useState(null);
+  const [showtrTable, setShowtrTable] = useState(false);
+  const [branchList, setBranchList] = useState([]);
+  const [view, setView] = useState([]);
+  const reports = [
+    { value: 'transaction', label: 'Total Transaction' },
+    { value: 'loan', label: 'Late Loan Installment' },
+    // Add more options as needed
+  ];
+
+
+  useEffect(()=>{
+    branch_fetch()
+      .then((branches)=>{
+        setBranchList(branches);
+      }).catch((error)=>{
+          console.error(error);
+      })
+  }, [])
+
+  //response must be mapped to the branches
+  
+  
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
+    title: "Account Number",
+    dataIndex: "accnum",
+    key: "accnum",
   },
   {
-    title: 'Chinese Score',
-    dataIndex: 'chinese',
-    sorter: {
-      compare: (a, b) => a.chinese - b.chinese,
-      multiple: 3,
-    },
-  },
-  {
-    title: 'Math Score',
-    dataIndex: 'math',
-    sorter: {
-      compare: (a, b) => a.math - b.math,
-      multiple: 2,
-    },
-  },
-  {
-    title: 'English Score',
-    dataIndex: 'english',
-    sorter: {
-      compare: (a, b) => a.english - b.english,
-      multiple: 1,
-    },
+    title: "Count",
+    dataIndex: "count",
+    key: "count",
   },
 ];
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    chinese: 98,
-    math: 60,
-    english: 70,
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    chinese: 98,
-    math: 66,
-    english: 89,
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    chinese: 98,
-    math: 90,
-    english: 70,
-  },
-  {
-    key: '4',
-    name: 'Jim Red',
-    chinese: 88,
-    math: 99,
-    english: 89,
-  },
-];
-const onChange = (pagination, filters, sorter, extra) => {
-  console.log('params', pagination, filters, sorter, extra);
-};
-const TotalTransactions = () => 
-<div className='page-container'>
-<div className='content-container'>
-    <Table 
-columns={columns} 
-dataSource={data} onChange={onChange} 
-title = {() => <h2>Total Transactions</h2>}
-scroll={{x:250}}/>
-</div>
-</div>
 
-export default TotalTransactions;
+
+  const handleClick= async() => {
+    if(reportType == 'transaction' && branch =='B001'){
+        console.log("Reached");
+        const data =await view_fetch();
+        setView(data);
+        setShowtrTable(true);
+    }
+    else{
+      setShowtrTable(false);
+    }
+
+  }
+  return (
+    <>
+    <div></div>
+    <div className = "centered-container">
+      <div className="title-container"><h2>Reports</h2></div>
+      <div className='input-container'>
+      <Select
+        className='select-container'
+        value={reportType}
+        onChange={(value) => setReportType(value)}
+        style={{ width: 200 }}
+        placeholder="Select Report Type"
+      >
+        {reports.map((option) => (
+          <Select.Option key={option.value} value={option.value}>
+            {option.label}
+          </Select.Option>
+        ))}
+      </Select>
+      <Select
+        className='select-container'
+        value={branch}
+        onChange={(value) => setBranch(value)}
+        style={{ width: 200 }}
+        placeholder="Select a Branch"
+      >
+        {branchList.map((option) => (
+          <Select.Option key={option.value} value={option.value}>
+            {option.label}
+          </Select.Option>
+
+        ))}
+      </Select>
+      
+
+    <Button type="primary" htmlType="submit" onClick={handleClick} className="generate-button">Generate</Button>
+    </div>
+    {showtrTable && (
+      <Table className='table-container'
+        dataSource={view}
+        columns={columns}
+        bordered
+      />
+    
+    )}
+    
+    </div>
+    
+    </>
+   
+    
+  )
+ 
+      }
+
+export default Report;
+
+
+
+
