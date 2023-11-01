fe506b1cca35cc57aade13f47cd623a907a25b29
NiroshanS21 <niroshans.21@uom.lk>
2023-11-01 17:38:13 +0530

    customer details

--- src/app/home/customers/[customerID]/page.js
+++ src/app/home/customers/[customerID]/page.js
@@ -1,7 +1,62 @@
 "use client";
+import React from "react";
+import "./style.css";
+const { getCustomers } = require("@/api/dataProvider");
+const { useState, useEffect } = require("react");
 
 const SingleCustomer = ({ params }) => {
-  return <h1>{params.customerID}</h1>;
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const res = await getCustomers();
+      if (res.status === 200) {
+        setData(res.data);
+      } else {
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
+  const customerData = data.find((customer) => customer.CustomerID === Number(params.customerID));
+
+  if (!customerData) {
+    return <p>Customer not found</p>;
+  }
+
+  return (
+    <div className="single-customer">
+      
+      <div className="customer-data">
+        <label>CustomerID:</label>
+        <input type="text" value={customerData.CustomerID} readOnly />
+        <label>Name:</label>
+        <input type="text" value={customerData.Name} readOnly />
+        <label>Email:</label>
+        <input type="text" value={customerData.Email} readOnly />
+        <label>CustomerType:</label>
+        <input type="text" value={customerData.CustomerType} readOnly />
+        <label>NIC / BR:</label>
+        <input type="text" value={customerData.NIC_BR} readOnly />
+        <label>Phone:</label>
+        <input type="text" value={customerData.Phone} readOnly />
+        <label>Address:</label>
+        <input type="text" value={customerData.Address} readOnly />
+        <label>Date of Birth:</label>
+        <input type="text" value={customerData.DOB} readOnly />
+      </div>
+    </div>
+  );
 };
 
 export default SingleCustomer;
