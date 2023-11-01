2eaee3d2ffb8b1be2a02bc6628884541b3a9c923
NiroshanS21 <niroshans.21@uom.lk>
2023-11-01 23:17:52 +0530

    update cutomer view

--- src/app/home/customers/[customerID]/page.js
+++ src/app/home/customers/[customerID]/page.js
@@ -38,22 +38,38 @@ const SingleCustomer = ({ params }) => {
   return (
     <div className="single-customer">
       <div className="customer-data">
-        <label>CustomerID:</label>
+        <label>Customer ID:</label>
         <input type="text" value={customerData.CustomerID} readOnly />
+
         <label>Name:</label>
         <input type="text" value={customerData.Name} readOnly />
+
+        <label>User Name:</label>
+        <input type="text" value={customerData.Username} readOnly />
+
         <label>Email:</label>
         <input type="text" value={customerData.Email} readOnly />
+
         <label>CustomerType:</label>
         <input type="text" value={customerData.CustomerType} readOnly />
+
         <label>NIC / BR:</label>
         <input type="text" value={customerData.NIC_BR} readOnly />
+
         <label>Phone:</label>
         <input type="text" value={customerData.Phone} readOnly />
+
         <label>Address:</label>
         <input type="text" value={customerData.Address} readOnly />
+
         <label>Date of Birth:</label>
         <input type="text" value={customerData.DOB} readOnly />
+
+        <label>Role:</label>
+        <input type="text" value={customerData.Role} readOnly />
+        
+        <label>UserID:</label>
+        <input type="text" value={customerData.UserID} readOnly />
       </div>
     </div>
   );
